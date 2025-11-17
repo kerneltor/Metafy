c92abc6ea38315f41b6436366c2c5e87f520fa1a
Ali Nayreez <alinayreez@gmail.com>
2025-11-17 17:59:09 +0330

    developing collapser across style.css and main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const collapsibles = document.querySelectorAll('.collapsible')
+const collapsers = document.querySelectorAll('.collapser')
+
 collapsibles.forEach((item) => {
+  addCollapsibleExtended(item)
+})
+
+collapsers.forEach((item) => {
+  addCollapsibleExtended(item)
+})
+
+function addCollapsibleExtended(item) {
   item.addEventListener('click', function () {
     this.classList.toggle('collapsible--expanded')
   })
-})
+}
